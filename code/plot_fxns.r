b9e611652791825e10d600a3fe68907f2eb114f9
Paul Cross <pcross@usgs.gov>
2018-06-15 15:59:55 -0600

    updated plot fxns

--- code/plot_fxns.r
+++ code/plot_fxns.r
@@ -15,25 +15,28 @@ plot.tots <- function(dat, ...){
     dplyr::rename(age = Var1, month = Var2, population = value,
                   category = L1) %>%
     filter(month %in% indices) %>%
-    mutate(year = (month-1)/12, sex = as.factor(str_sub(category, -1)))
+    mutate(year = (month-1)/12, sex = as.factor(str_sub(category, -1)),
+           disease = "no")
+  dat.lon$disease[str_sub(dat.lon$category, 1,1) == "I"] = "yes"
 
   # summarize by year and sex
   dat.sum <- dat.lon %>%
-    group_by(year, sex) %>%
+    group_by(year, disease) %>%
     summarize(n = sum(population)) %>%
-    spread(key = sex, value = n) %>%
-    mutate(tots = f + m)
+    spread(key = disease, value = n) %>%
+    mutate(tots = no + yes)
 
   #plot
   par(mar = c(4,5, 1,1))
   plot(dat.sum$year, dat.sum$tots, ...)
-  lines(dat.sum$year, dat.sum$f, col = "red", lwd = 2)
-  lines(dat.sum$year, dat.sum$m, col = "blue", lwd = 2)
-
+  lines(dat.sum$year, dat.sum$yes, col = "red", lwd = 2)
+  lines(dat.sum$year, dat.sum$no, col = "blue", lwd = 2)
+  legend("topright", c("total", "infected", "healthy"),
+         col = c("black", "red", "blue"), lwd = 2)
 }
 
 # plot all ages all months
-plot.all <- function(dat, ...){
+plot.all <- function(dat, years.only, ...){
   # INPUT
   # dat = list of the output matrices
   # OUTPUT
@@ -41,40 +44,23 @@ plot.all <- function(dat, ...){
   require(reshape2)
   require(ggplot2)
 
-  dat.long <- melt(dat)
-  names(dat.long) <- c("age", "month", "population", "category")
-  dat.long$category <- as.factor(dat.long$category)
-
-  plot1 <- ggplot(dat.long, aes(month, population, color = age)) +
-    geom_point() + facet_wrap(~category, ncol = 4)
-  plot1
-}
-
-# plot all ages only the start of the years
-plot.all.yr <- function(dat){
-
-  # INPUT
-  # dat = list of the output matrices
-  # OUTPUT
-  # plot of the population totals split by age, sex, disease category
-  require(reshape2)
-  require(tidyverse)
-
   # create the indices for the start of each year
-  yr.indices <- which(seq(1,dim(dat[[1]])[2]) %% 12 == 1)
+  indices <- which(seq(1,dim(dat[[1]])[2]) %% 12 == 1)
 
   # organize the data into long form
   dat.lon <- melt(dat) %>%
-   dplyr::rename(age = Var1, month = Var2, population = value,
-                        category = L1) %>%
-    filter(month %in% yr.indices) %>%
-    arrange(category, age, month)
-  dat.lon$category <- as.factor(dat.lon$category)
-  dat.lon$year <- (dat.lon$month-1)/12
+    dplyr::rename(age = Var1, month = Var2, population = value,
+                  category = L1) %>%
+    mutate(year = (month-1)/12, sex = as.factor(str_sub(category, -1)),
+         disease = "no")
+  dat.lon$disease[str_sub(dat.lon$category, 1,1) == "I"] = "yes"
 
-  #plot it
+  if(years.only == TRUE){
+    dat.lon <- dat.lon %>%
+      filter(month %in% indices)}
+
   plot1 <- ggplot(dat.lon, aes(year, population, color = age)) +
-    geom_point() + facet_wrap(~category, ncol = 4)
+    geom_point() + facet_wrap(~disease + sex, ncol = 2)
   plot1
 }
 
@@ -110,8 +96,8 @@ plot.prev <- function(dat, ...){
   plot(dat.sum$year, dat.sum$prev, ...)
 }
 
-# plot the doe:fawn
-plot.fawn.doe <- function(dat, ...){
+# plot the fawn:adult
+plot.fawn.adult <- function(dat, ...){
   # INPUT
   # dat = list of the output matrices
   # OUTPUT
@@ -138,11 +124,43 @@ plot.fawn.doe <- function(dat, ...){
     group_by(year, age.cat) %>%
     summarize(n = sum(population)) %>%
     spread(key = age.cat, value = n) %>%
-    mutate(fawn.doe = fawn / adult)
+    mutate(fawn.adult = fawn / adult)
 
-  plot(dat.sum$year, dat.sum$fawn.doe, ...)
+  plot(dat.sum$year, dat.sum$fawn.adult, ...)
 
 }
 
-
 # plot buck:doe
+plot.buck.doe <- function(dat, ...){
+  # INPUT
+  # dat = list of the output matrices
+  # OUTPUT
+  # plot of the prevalence
+  require(reshape2)
+  require(tidyverse)
+
+  # create the indices for the start of each year
+  indices <- which(seq(1, dim(dat[[1]])[2]) %% 12 == 8) # December of every year
+
+  # organize the data into long form
+  dat.lon <- melt(dat) %>%
+    dplyr::rename(age = Var1, month = Var2, population = value,
+                  category = L1) %>%
+    filter(month %in% indices) %>%
+    mutate(year = (month-1)/12, sex = as.factor(str_sub(category, -1)))
+
+  dat.lon$age.cat <- "adult"
+  dat.lon$age.cat[dat.lon$age == 1] <- "fawn"
+
+
+  # summarize by year and disease status, calculate the prevalence
+  dat.sum <- dat.lon %>%
+    group_by(year, sex, age.cat) %>%
+    summarize(n = sum(population)) %>%
+    unite(sex.age, sex, age.cat) %>%
+    spread(key = sex.age, value = n) %>%
+    mutate(buck.doe = m_adult / f_adult)
+
+  plot(dat.sum$year, dat.sum$buck.doe, ...)
+
+}
