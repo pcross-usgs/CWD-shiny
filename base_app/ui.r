8746e0ed85a27de5c2304467960596b26600dcde
Paul Cross <pcross@usgs.gov>
2020-03-16 13:25:23 -0600

    minor text edit

--- base_app/ui.r
+++ base_app/ui.r
@@ -10,7 +10,7 @@ ui <- fluidPage(theme = "common.css",
                 Wildlife and Parks"),
                            windowTitle = "CWD model"),
 
-                navbarPage("Chronic Wasting Disease",
+                navbarPage("Base CWD model",
                            tabPanel("Description",
                                     withMathJax(includeHTML("description_combo.html"))),
                            tabPanel("Deterministic model",
