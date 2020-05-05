8b5a06a86322153b53c0f131415c1f32695033ed
Paul Cross <pcross@usgs.gov>
2020-05-05 15:01:45 -0600

    found an error that only arose when n.years is small.

--- CWD/wiw_app/plot_compare_all_stoch.R
+++ CWD/wiw_app/plot_compare_all_stoch.R
@@ -15,35 +15,35 @@
 #' @importFrom cowplot plot_grid
 #' @importFrom stringr str_sub
 #' @importFrom forcats fct_recode
-#' @examples 
-#' params.a <- list(fawn.an.sur = 0.6, juv.an.sur = 0.8, ad.an.f.sur = 0.95, 
-#' ad.an.m.sur = 0.9, fawn.repro = 0, juv.repro = 0.6, ad.repro = 1, 
+#' @examples
+#' params.a <- list(fawn.an.sur = 0.6, juv.an.sur = 0.8, ad.an.f.sur = 0.95,
+#' ad.an.m.sur = 0.9, fawn.repro = 0, juv.repro = 0.6, ad.repro = 1,
 #' hunt.mort.fawn = 0.01, hunt.mort.juv.f = 0.1, hunt.mort.juv.m = 0.1,
 #' hunt.mort.ad.f = 0.2, hunt.mort.ad.m = 0.2, ini.fawn.prev = 0.02,
 #' ini.juv.prev = 0.03, ini.ad.f.prev = 0.04,  ini.ad.m.prev = 0.04,
 #' n.age.cats = 12,  p = 0.43, env.foi = 0,  beta.f = 0.15,  beta.m = 0.15,
-#' theta = 1, n0 = 2000, n.years = 10, rel.risk = 1.0, 
+#' theta = 1, n0 = 2000, n.years = 10, rel.risk = 1.0,
 #' repro.var = 0.005, fawn.sur.var = 0.005, sur.var = 0.005, hunt.var = 0.005)
-#' 
-#' params.b <- list(fawn.an.sur = 0.6, juv.an.sur = 0.8, ad.an.f.sur = 0.95, 
-#' ad.an.m.sur = 0.9, fawn.repro = 0, juv.repro = 0.6, ad.repro = 1, 
+#'
+#' params.b <- list(fawn.an.sur = 0.6, juv.an.sur = 0.8, ad.an.f.sur = 0.95,
+#' ad.an.m.sur = 0.9, fawn.repro = 0, juv.repro = 0.6, ad.repro = 1,
 #' hunt.mort.fawn = 0.01, hunt.mort.juv.f = 0.1, hunt.mort.juv.m = 0.1,
 #' hunt.mort.ad.f = 0.05, hunt.mort.ad.m = 0.6, ini.fawn.prev = 0.02,
 #' ini.juv.prev = 0.03, ini.ad.f.prev = 0.04,  ini.ad.m.prev = 0.04,
 #' n.age.cats = 12,  p = 0.43, env.foi = 0,  beta.f = 0.15,  beta.m = 0.15,
-#' theta = 1, n0 = 2000, n.years = 10, rel.risk = 1.0, 
+#' theta = 1, n0 = 2000, n.years = 10, rel.risk = 1.0,
 #' repro.var = 0.005, fawn.sur.var = 0.005, sur.var = 0.005, hunt.var = 0.005)
 #
 #' out.a <- cwd_stoch_wrapper(params.a, nsims = 20)
 #' out.b <- cwd_stoch_wrapper(params.b, nsims = 20)
-#' 
+#'
 #' plot_compare_all_stoch(out.a, out.b)
 #'
 #' @export
 
 plot_compare_all_stoch <- function(outa, outb){
   if(missing(outa)==TRUE) warning("missing scenario a data to plot")
-  if(missing(outb)==TRUE) warning("missing scenario b data to plot")  
+  if(missing(outb)==TRUE) warning("missing scenario b data to plot")
   outcount <- list(outa$counts, outb$counts)
   outcount <- melt(outcount, id = c("age", "month", "population", "category",
                                     "year", "sex", "disease", "sim")) %>%
@@ -79,12 +79,12 @@ plot_compare_all_stoch <- function(outa, outb){
     summarize(n = sum(population))
 
   last.hunted <- hunted %>%
-    filter(round(year, 0) == max(round(year, 0))) %>%
+    filter(year > max(year)-1) %>%
     group_by(sim, scenario) %>%
     summarize(n = sum(population))
 
   males.last.hunted <- hunted %>%
-    filter(round(year, 0) == max(round(year, 0))) %>%
+    filter(year > max(year)-1) %>%
     filter(sex == "m") %>%
     group_by(sim, scenario) %>%
     summarize(n = sum(population))
